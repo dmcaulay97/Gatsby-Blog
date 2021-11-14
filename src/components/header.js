a35d8cff008300964ea9b241b8e575cb98d0c93c
Dylan McAulay <dmcaulay97@gmail.com>
2021-11-14 10:50:37 -0500

    added layout and heder styles

--- src/components/header.js
+++ src/components/header.js
@@ -6,21 +6,25 @@ import * as headerStyles from "./header.module.scss"
 const Header = () => {
 
     return (
-        <header>
-            <h1>Dylan McAulay</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link to="/" className={headerStyles.title}>
+                    Dylan McAulay
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link className={headerStyles.link} to="/">Home Page</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/">Home Page</Link>
                     </li>
                     <li>
-                        <Link className={headerStyles.link} to="/about">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/about">About</Link>
                     </li>
                     <li>
-                        <Link className={headerStyles.link} to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link className={headerStyles.link} to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.active} to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
